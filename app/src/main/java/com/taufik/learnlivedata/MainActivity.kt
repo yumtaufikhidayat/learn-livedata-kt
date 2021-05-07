4f2cd3a61a6d0674b1acba4a767b6a23639900f7
yumtaufikhidayat <yumtaufik1997@gmail.com>
2021-05-08 05:37:02 +0700

    Updating project.

--- app/src/main/java/com/taufik/learnlivedata/MainActivity.kt
+++ app/src/main/java/com/taufik/learnlivedata/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.taufik.learnlivedata
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.taufik.learnlivedata.databinding.ActivityMainBinding
+import com.taufik.learnlivedata.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var mLiveDataTimerViewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setLiveData()
+
+        subscribe()
+    }
+
+    private fun setLiveData() {
+        mLiveDataTimerViewModel = ViewModelProvider(this)[MainViewModel::class.java]
+    }
+
+    private fun subscribe() {
+        val elapsedTimeObserver = Observer<Long?> { aLong ->
+            val newText = this.resources.getString(R.string.tvSeconds, aLong)
+            binding.timerTextview.text = newText
+        }
+
+        mLiveDataTimerViewModel.getElapsedTime().observe(this, elapsedTimeObserver)
     }
 }
